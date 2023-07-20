8795c44fe7d8572d52051e6e46049ee0935695b9
Leslie Neema Keza <leslieneemakeza@gmail.com>
2023-07-19 18:55:06 -0700

    poiuyt

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,19 +16,19 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				printf("%d, ", i);
+				printf(", %d", i);
 			else if (i == 98)
-				printf("%d\n", i);
+				printf(", %d", i);
 		}
 	}
-	else if (n >= 98)
+	else
 	{
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				printf("%D, ", j);
+				printf(", %d", j);
 			else if (j == 98)
-				printf("%d\n", j);
+				printf(", %d", j);
 		}
 	}
 }
