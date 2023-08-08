3bbf38f9b60c191348319827e234566b4c0f5703
Leslie Neema Keza <leslieneemakeza@gmail.com>
2023-08-08 05:50:41 -0700

    diella

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- * argstostr - main entry
+ * argstostr - Main entry
  * @ac: int input
  * @av: double pointer array
  * Return: 0
  */
 
-char *argstostr(int ac, int **av)
+char *argstostr(int ac, char **av)
 {
 	int i, n, r = 0, l = 0;
 	char *str;
