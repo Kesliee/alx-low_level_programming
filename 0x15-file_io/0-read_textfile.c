1644bf80d30eb903424a4e34ec17d27dc17014a3
Kesliee <leslieneemakeza@gmail.com>
2023-11-03 17:06:51 -0700

    huuu

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,11 +7,8 @@
 
 /**
  * read_textfile - this reads a text file and prints it to the POSIX stdo
- *
  * @filename: name of the file to read
- *
  * @letters: number of letters it should read and print
- *
  * Return: actual number of letters it could read and print
  */
 
